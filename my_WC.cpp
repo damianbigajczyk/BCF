fe08e5dcb12877e520603234c5015c8d7bb17ac9
Damian Bigajczyk <d.bigajczyk@gmail.com>
2021-09-28 14:30:35 +0200

    add DirectoryInfo class

--- my_WC.cpp
+++ my_WC.cpp
@@ -5,10 +5,24 @@
 
 namespace fs = std::experimental::filesystem;
 
-void state(const fs::path& p) {
-	if (fs::is_regular_file(p))
+class DirectoryInfo {
+public:
+	DirectoryInfo();
+	void incrementNumOfFiles();
+	uint32_t getNumOfFiles();
+
+private:
+	uint32_t numOfFiles = 0;
+	uint32_t totalNumOfLines = 0;
+	uint32_t numOfNonEmptyLines = 0;
+	uint32_t numOfEmptyLines = 0;
+};
+	
+void state(DirectoryInfo& dir, const fs::path& p) {
+	if (fs::is_regular_file(p)) {
 		std::cout << "file\t";
-	else if (fs::is_directory(p))
+		dir.incrementNumOfFiles();
+	} else if (fs::is_directory(p))
 		std::cout << "directory\t";
 	else
 		std::cout << "\t" << p;
@@ -20,21 +34,26 @@ int main( int argc, char **argv ) {
 		std::cout << "Current dir: " << dir << std::endl;
 		return 0;
 	}
+	DirectoryInfo firstDir{};
+
 	while (argc-- > 1) {
 		if (fs::is_regular_file(argv[argc]))
 			std::cout << argv[argc] << " is a regular file\n";
 		else if (fs::is_directory(argv[argc])) {
 			for (auto const& dir_entry: fs::recursive_directory_iterator{argv[argc]}) {
-				state(dir_entry.path());
+				state(firstDir, dir_entry.path());
 				std::cout << dir_entry << std::endl;
 			}
 		} else
 			std::cout << "neither\n";
 	}
-
-
-	
-
-
+	std::cout << "Number of files: " << firstDir.getNumOfFiles() << std::endl;
 	return 0;
+}
+DirectoryInfo::DirectoryInfo() {}
+void DirectoryInfo::incrementNumOfFiles() {
+	++numOfFiles;
+}
+uint32_t DirectoryInfo::getNumOfFiles() {
+	return numOfFiles;
 }
