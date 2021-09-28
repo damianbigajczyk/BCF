4f3fb288910048befdc8ada824b7fb56b8c86ac2
Damian Bigajczyk <d.bigajczyk@gmail.com>
2021-09-28 13:33:10 +0200

    opisy

--- my_WC.cpp
+++ my_WC.cpp
@@ -5,6 +5,15 @@
 
 namespace fs = std::experimental::filesystem;
 
+void state(const fs::path& p) {
+	if (fs::is_regular_file(p))
+		std::cout << "file\t";
+	else if (fs::is_directory(p))
+		std::cout << "directory\t";
+	else
+		std::cout << "\t" << p;
+}
+
 int main( int argc, char **argv ) {
 	if (argc < 2) {
 		const auto dir = fs::current_path();
@@ -15,9 +24,10 @@ int main( int argc, char **argv ) {
 		if (fs::is_regular_file(argv[argc]))
 			std::cout << argv[argc] << " is a regular file\n";
 		else if (fs::is_directory(argv[argc])) {
-			std::cout << argv[argc] << " is a directory\n";
-			for (auto const& dir_entry: fs::recursive_directory_iterator{argv[argc]})
+			for (auto const& dir_entry: fs::recursive_directory_iterator{argv[argc]}) {
+				state(dir_entry.path());
 				std::cout << dir_entry << std::endl;
+			}
 		} else
 			std::cout << "neither\n";
 	}
