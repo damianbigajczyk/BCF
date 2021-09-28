5601914c0a625407b2cd6a6f0841b3e3fe99ee46
Damian Bigajczyk <d.bigajczyk@gmail.com>
2021-09-28 12:57:25 +0200

    add library filesystem

--- my_WC.cpp
+++ my_WC.cpp
@@ -1,22 +1,28 @@
 #include <iostream>
 #include <string>
 #include <fstream>
+#include <experimental/filesystem>
 
-using namespace std;
+namespace fs = std::experimental::filesystem;
 
 int main( int argc, char **argv ) {
-	if (argc < 2)
+	if (argc < 2) {
+		const auto dir = fs::current_path();
+		std::cout << "Current dir: " << dir << std::endl;
 		return 0;
-
-	while (argc-- > 1 ) {
-		ifstream ist {argv[argc]};
-		if (!ist) {
-			cout << "Can't open file\n";
-		}
-		char c;
-		while (ist >> c)
-			cout << c;
 	}
+	while (argc-- > 1) {
+		if (fs::is_regular_file(argv[argc]))
+			std::cout << argv[argc] << "is a regular file\n";
+		else if (fs::is_directory(argv[argc]))
+			std::cout << argv[argc] << "is a directory\n";
+		else
+			std::cout << "neither\n";
+	}
+
+
+	
+
 
 	return 0;
 }
