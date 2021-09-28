7f97f9b75a4a2c15a91120e6143fc62d6fba55bb
Damian Bigajczyk <d.bigajczyk@gmail.com>
2021-09-28 09:26:51 +0200

    read file

--- my_WC.cpp
+++ my_WC.cpp
@@ -1,11 +1,22 @@
 #include <iostream>
+#include <string>
+#include <fstream>
 
 using namespace std;
 
 int main( int argc, char **argv ) {
+	if (argc < 2)
+		return 0;
 
-
-	cout << "Hello" << endl;
+	while (argc-- > 1 ) {
+		ifstream ist {argv[argc]};
+		if (!ist) {
+			cout << "Can't open file\n";
+		}
+		char c;
+		while (ist >> c)
+			cout << c;
+	}
 
 	return 0;
 }
